c3ceebe9a747b814557156279118847afa899172
Ricardo Wright <rcrd_wright@hotmail.com>
2025-02-15 20:10:04 -0500

    update the array print method

--- Payroll.java
+++ Payroll.java
@@ -1,6 +1,6 @@
 //Abbligale Ried - ID#2301010638
 //Kwame Harriott - ID#2301011566
-//Shemoy Shay -  ID#2201011505
+//Shemoy Shay    - ID#2201011505
 //Ricardo Wright - ID#2201010833
 
 import java.util.ArrayList;
@@ -8,21 +8,30 @@ import java.util.ArrayList;
 
 public class Payroll {
     public static void main(String[] args) {
+
         // Instantiate a SalesRep staffMemeber object
+        System.out.println("One Instance of a SalesRep staff Member ");
         SalesRep salesRep = new SalesRep("Ricardo", "Smith", "Acct1", 40, 500);
 
-
         // Invoke the display method to print a SalesRep StaffMember data values
+        salesRep.display();
+
+        System.out.println();
+        System.out.println("Printing Array String of SalesRep Staff Members");
+
         ArrayList<SalesRep> salesReps = new ArrayList<>();
-        salesReps.add(new SalesRep("Jessia","Miller",1002,45, 5620));
-        salesReps.add(new SalesRep("Calvin","Street",1052,95, 6620));
-        salesReps.add(new SalesRep("Kwamie","Clarke",1006,65, 9620));
-        salesReps.add(new SalesRep("Nicki","Bell",   1006,75, 6620));
-        salesReps.add(new SalesRep("Hector","Douglas",1002,75, 5620));
-        salesReps.add(new SalesRep("Jessie","Mills",1056,95, 5620));
+        salesReps.add(new SalesRep("Jessia","Miller","1002",45, 5620));
+        salesReps.add(new SalesRep("Calvin","Street","1052",95, 6620));
+        salesReps.add(new SalesRep("Kwamie","Clarke","1006",65, 9620));
+        salesReps.add(new SalesRep("Nicki","Bell",   "1006",75, 6620));
+        salesReps.add(new SalesRep("Hector","Douglas","1002",75, 5620));
+        salesReps.add(new SalesRep("Jessie","Mills","1056",95, 5620));
 
-for (salesReps rep: salesRep) salesRep.display();
+        for (SalesRep rep: salesReps) {
+            System.out.println("*****************************");
+            rep.display();
 
+       }
     }
 
 }
